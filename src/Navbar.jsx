3c06d568ab1846bd2bd1be0c10ef294a1b57ab27
seohyeon-lee-2025 <seohyeonlee2020@gmail.com>
2025-05-21 07:07:44 +0900

    Navbar

---
+++ src/Navbar.jsx
@@ -0,0 +1,11 @@
+export default Navbar = () => {
+	return <>
+	<nav className="nav">
+		<ul>
+			<li><a href="#">Personal</a></li>
+			<li><a href="#">TSL Graphics</a></li>
+			<li><a href="#">Before 2020</a></li>
+		</ul>
+	</nav>
+	</>
+}
