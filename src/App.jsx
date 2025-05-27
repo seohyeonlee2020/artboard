935cb9bbff6331532bffc89867724b6e7aa6d2f9
seohyeon-lee-2025 <seohyeonlee2020@gmail.com>
2025-05-27 15:47:13 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,10 @@ import './App.css';
 import React from "react"
 import { Routes, Route } from "react-router-dom";
 
-import Artcard from './components/Artcard'
 import Header from './components/Header'
 
 import PageTemplate from './pages/PageTemplate'
 import Projects from "./pages/Projects";
-
-/* page imports
-import All from './pages/All'
-import Projects from './pages/Projects'
-import Archive from './pages/commArchive' */
 
 const App = () => {
 return (
